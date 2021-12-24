6fcb48b3d2d0547b69b634b0a85478079ab7c319
med033 <medbm23@gmail.com>
2021-12-24 04:04:25 +0100

    card

---
+++ src/app/components/mesures-table/mesures-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mesures-table',
+  templateUrl: './mesures-table.component.html',
+  styleUrls: ['./mesures-table.component.css']
+})
+export class MesuresTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
